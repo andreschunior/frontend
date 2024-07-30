190ec6f6d238bb5efce6ed31b5f94f48a23b1456
andreschunior <andres23540575@gmail.com>
2024-07-30 12:45:57 -0400

    actualizacion proyecto final, se agregaron las tarjetas del request para que imprimieran los datos que solicitan del endpoint del back de relevamientos, se hicieron cambios menores en titulos de la pagina

--- proyectofinal-henry/src/Components/Dashboard/Main/Main.tsx
+++ proyectofinal-henry/src/Components/Dashboard/Main/Main.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import UserProfile from '../UserProfile/UserProfile';
-
+import React from "react";
+import UserProfile from "../UserProfile/UserProfile";
 
 const Main = ({ page }: { page: string }) => {
   const router = page.charAt(0).toUpperCase() + page.slice(1);
   const ComponentToRender = React.lazy(() => import(`./${router}/${router}`));
   return (
-      <>
-          {page === 'userProfile' ? <UserProfile /> : <ComponentToRender />}
-      </>
-      
+    <>{page === "userProfile" ? <UserProfile /> : <ComponentToRender />}</>
   );
 };
 
 export default Main;
-
