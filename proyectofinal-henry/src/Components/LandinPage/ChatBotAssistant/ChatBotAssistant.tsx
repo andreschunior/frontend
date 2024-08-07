b16ea37dee50390c0de09a453eb1b141bcce0929
coliveramispireta <coliveramispireta@gmail.com>
2024-08-07 15:40:56 -0500

    chatbot

--- proyectofinal-henry/src/Components/LandinPage/ChatBotAssistant/ChatBotAssistant.tsx
+++ proyectofinal-henry/src/Components/LandinPage/ChatBotAssistant/ChatBotAssistant.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
-import { steps } from './steps';
+import { steps } from '../../../helpers/steps';
 
 const DiseñoChat = {
   background: '#f5f8fb',
--- proyectofinal-henry/src/helpers/sidebarOptions.ts
+++ proyectofinal-henry/src/helpers/sidebarOptions.ts
@@ -51,7 +51,7 @@ export const sidebarOptionsAdmin = [
             options:[]
         },
         {
-            rolName: "Chatbotview",
+            rolName: "Asistente Virtual",
             icon: "M8 7V6a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1M3 18v-7a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z",
             router: true,
             options:[],
@@ -100,6 +100,12 @@ export const sidebarOptionsAdmin = [
         icon: "M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z",
         router: true,
         options:[]
+    },
+    {
+        rolName: "Asistente Virtual",
+        icon: "M8 7V6a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1M3 18v-7a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z",
+        router: true,
+        options:[],
     },
 ]
 
