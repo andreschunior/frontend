86b38f4dc0ca7a7324d17ffa76dd16332a752cc2
andreschunior <andres23540575@gmail.com>
2024-08-07 16:38:47 -0400

    actualizacion proyecto final se agrego la vista de usuario de mi plan, tambien la opcion de detalles y de cambiar de plan, se agrego los planes a la landingpage con datos del backend

--- proyectofinal-henry/src/Components/Dashboard/Main/Home/Home.tsx
+++ proyectofinal-henry/src/Components/Dashboard/Main/Home/Home.tsx
@@ -3,7 +3,7 @@ import { useSidebarContext } from "@/context/SidebarContext";
 import React from "react";
 import Loading from "@/Components/Dashboard/Loading/Loading";
 import { useAuth } from "@/context/AuthContext";
-
+import { PlanActual } from "../Plan/PlanActual";
 
 const Home = () => {
   const { btnFixed } = useSidebarContext();
@@ -18,15 +18,16 @@ const Home = () => {
           btnFixed ? "ml-[270px]" : "ml-24"
         }`}
       >
-
- 
-                        <h1 className="text-2xl font-bold text-blue-900 mt-10 dark:text-blue-400/70">
-                        {roles?.includes("admin") ? "Bienvenido a tu plataforma de Administración!" : "Bienvenido a tu plataforma de Personal!" }
-                      </h1>
-                      <h2 className="text-3xl text-gray-400 dark:text-orange-300/80">
-                      {roles?.includes("admin") ? "Dashboard Administrativo" : "Dashboard de Usuario" }
-                        
-                      </h2>
+        <h1 className="text-2xl font-bold text-blue-900 mt-10 dark:text-blue-400/70">
+          {roles?.includes("admin")
+            ? "Bienvenido a tu plataforma de Administración!"
+            : "Bienvenido a tu plataforma de Personal!"}
+        </h1>
+        <h2 className="text-3xl text-gray-400 dark:text-orange-300/80">
+          {roles?.includes("admin")
+            ? "Dashboard Administrativo"
+            : "Dashboard de Usuario"}
+        </h2>
 
         <br />
 
